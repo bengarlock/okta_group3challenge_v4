05823c31b59f0243cc79fd16c10e011f78b9787d
Ben Garlock <ben.garlock@gmail.com>
2021-03-03 15:04:39 -0500

    finally?

--- patientportal/patientportal/settings.py
+++ patientportal/patientportal/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from .passwords import postgrest_password, return_secret_key
+from passwords import return_secret_key, postgrest_password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
